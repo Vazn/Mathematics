d7667ecfd22e91efacbffa234d586f29508f2c1f
vazn <cavazzoni.chris@protonmail.com>
2024-10-28 20:02:04 +0100

    Adding manim cfg

---
+++ Python/Manim/customConfig.py
@@ -0,0 +1,51 @@
+AXISCONFIG = {
+    'x_range':[-25, 25, 1],
+    'y_range':[-25, 25, 1],
+    'x_length':50,
+    'y_length':50,
+    "color": '#E8E8E8',
+    "stroke_width": 4,
+    "stroke_opacity": 1,
+}
+
+PLANECONFIG = {
+    'x_range':[-25, 25, 1],
+    'y_range':[-25, 25, 1],
+    'x_length':50,
+    'y_length':50,
+    "color": '#E8E8E8',
+    "stroke_width": 4,
+    "stroke_opacity": 1,
+
+    'faded_line_ratio' :2,
+    'background_line_style':{
+        "stroke_width": 3,
+        "stroke_opacity": 0.35,
+        "stroke_color": '#FFFFFF'
+    },
+    'faded_line_style':{
+        "stroke_width": 3,
+        "stroke_opacity": 0.10,
+        "stroke_color": '#FFFFFF'
+    },
+}
+
+POLARPLANECONFIG = {
+    'size': 30,
+    'radius_max': 15,
+    'radius_step':1,
+    'azimuth_step': 8,
+    'faded_line_ratio' :3,
+    'background_line_style':{
+        "stroke_width": 4,
+        "stroke_opacity": 0.95,
+        "stroke_color": '#6E8CB6'
+    },
+    'faded_line_style':{
+        "stroke_width": 3,
+        "stroke_opacity": 0.3,
+        "stroke_color": '#6E8CB6'
+    },
+}
+
+SPACECONFIG = {}
